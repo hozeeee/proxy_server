e56757140f3d5138d8d5ce789d80f0d44dfe57ba
hozeeee <15820676504@139.com>
2024-05-28 22:40:42 +0800

    feat: init

---
+++ server/src/config/config.default.ts
@@ -0,0 +1,14 @@
+import { MidwayConfig, MidwayAppInfo } from '@midwayjs/core';
+
+export default (appInfo: MidwayAppInfo) => {
+  return {
+    // use for cookie sign key, should change to your own and keep security
+    keys: appInfo.name + '_1716906401826_9976',
+    egg: {
+      port: 7001,
+    },
+    // security: {
+    //   csrf: false,
+    // },
+  } as MidwayConfig;
+};
