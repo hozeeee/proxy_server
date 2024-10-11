c1a9adb992244c8157c01d2876dcf7ebbb560d83
hozeeee <15820676504@139.com>
2024-10-11 15:21:30 +0800

    feat: 删除 forward_end/src/index.ts 文件

--- forward_end/src/index.ts
+++
@@ -1,8 +0,0 @@
-
-/**
- * 其他程序通过 SDK 的方式引入。
- */
-
-export * from './controller/http_proxy.controller';
-export * from './controller/axios_request.controller';
-export * from './controller/tigervnc_forward.controller';
