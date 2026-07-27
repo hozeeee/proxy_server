01fcce592a1557955b992babb0885dded1cc0122
hozeeee <15820676504@139.com>
2026-07-27 17:04:41 +0800

    feat: 代理客户端开启 setDefaultAutoSelectFamily 。

--- forward_end/src/end_manager.ts
+++ forward_end/src/end_manager.ts
@@ -1,4 +1,15 @@
 import { startClient } from './base/start_client';
+import net from 'net';
+
+/**
+ * 开启后，同时尝试 IPv4 和 IPv6 的通道，那个先通用哪个。
+ * 对于 node@20 及以上的版本，这个是默认开启的，
+ * 但 18 是关的（只按 DNS 返回的第一个地址去连，通常是 IPv6），
+ * 导致如果 IPv6 不通，就会卡死。
+ */
+try {
+    net.setDefaultAutoSelectFamily(true);
+} catch { }
 
 
 const SERVER_HOST = process.env.SERVER_HOST;
